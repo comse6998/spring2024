24f73f2aff7d243b1629f2e42c4534fcba7a7520
lafis002 <l.fischer00@yahoo.de>
2024-01-24 11:52:54 -0500

    Test HW1

--- Src/blas/dcopy.cc
+++ Src/blas/dcopy.cc
@@ -40,18 +40,18 @@ namespace CDC8600
 	    jmpp(2, L1)		// if X2 (incx) > 0 goto L1
 	    idzkj(5, 0)		// X5 (ix) = -X0 (n)
 	    isjkj(5, 1)		// X5 (ix) = X5(-n) + 1
-            ipjkj(5, 2)		// X5 (ix) = X5 (-n+1) * X2 (incx)
+        ipjkj(5, 2)		// X5 (ix) = X5 (-n+1) * X2 (incx)
 LABEL(L1)   jmpp(4, loop)	// if X4 (incy) > 0 goto loop
 	    idzkj(6, 0)		// X6 (iy) = -X0 (n)
 	    isjkj(6, 1)		// X6 (iy) = X6(-n) + 1
-            ipjkj(6, 4)		// X6 (iy) = X6 (-n+1) * X4 (incy)
+        ipjkj(6, 4)		// X6 (iy) = X6 (-n+1) * X4 (incy)
 LABEL(loop) jmpz(0, end)	// if X0 (n) = 0 goto end
-            rdjki(7, 1, 5)	// X7 (tmp) = MEM[X1 (x) + X5 (ix)]
+        rdjki(7, 1, 5)	// X7 (tmp) = MEM[X1 (x) + X5 (ix)]
 	    sdjki(7, 3, 6)	// MEM[X3 (y) + X6 (iy)] = X7 (tmp)
-            isjki(5, 5, 2)	// X5 (ix) = X5 (ix) + X2 (incx)
+        isjki(5, 5, 2)	// X5 (ix) = X5 (ix) + X2 (incx)
 	    isjki(6, 6, 4)	// X6 (iy) = X6 (iy) + X4 (incy)
 	    idjkj(0, 1)		// X0 (n) = X0 (n) - 1
-            jmp(loop)
+        jmp(loop)
 LABEL(end)  jmpk(15, 1)		// return to X15 (calling address) + 1
 	}
     } // namespace BLAS
