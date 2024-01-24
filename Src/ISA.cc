7496720962bb05ab811380cadf836ac3664b4e3d
Jose Moreira <jmoreira@us.ibm.com>
2024-01-24 18:27:44 -0500

    Added rdKj instruction and general register declarations

--- Src/ISA.cc
+++ Src/ISA.cc
@@ -99,5 +99,30 @@ namespace CDC8600
 	{
 	    assert(false);
 	}
+
+	void rdKj
+	(
+	    uint8_t Xj,
+	    uint32_t K
+	)
+	{
+	    assert(Xj < 16);
+	    assert(K < 1024*1024);
+
+	    if (K < PROC.FL().u()*256)
+	    {
+		// Good
+		uint32_t addr = PROC.RA().u()*256 + K;	// Architected address
+		assert(addr < params::MEM::N);		// Check against hardware limit
+		PROC.X(Xj) = MEM[addr];
+	    }
+	    else
+	    {
+		// Bad
+		PROC.cond()(2) = true;
+		PROC._XA = PROC.XA().u();
+	    }
+	}
+
    } // namespace instructions
 } // namespace CDC8600
