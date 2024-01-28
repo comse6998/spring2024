2119102fb52f2109958bdc5975dfbbdff25778bb
Teng Jiang <jiangteng0403@gmail.com>
2024-01-28 01:38:13 -0500

    Basic implementations; passed tests. TODO: Corner cases. Check for inputs.

--- Src/ISA.cc
+++ Src/ISA.cc
@@ -20,7 +20,9 @@ namespace CDC8600
 	    uint8_t Xj
 	)
 	{
-	    assert(false);
+	    assert(Xj < 16);
+		if (PROC.X(Xj).i() > 0) return true;
+		else return false;
 	}
 	
         void xkj
@@ -41,14 +43,29 @@ namespace CDC8600
 	    uint8_t Xk
 	)
 	{
-		cout << "X(Xj)" << PROC.X(Xj).u() << endl;
-		cout << "X(Xk)" << PROC.X(Xk).u() << endl;
-	    assert(Xi < 16);
+		// cout << "rdjki, Xi: " << unsigned(Xi) << "Xj: " << unsigned(Xj) << "Xk: " << unsigned(Xk) << endl;
+		// cout << "Xi: " << unsigned(Xi) << endl;
+		// cout << "Xj: " << unsigned(Xj) << endl;
+		// cout << "Xk: " << unsigned(Xk) << endl;
+		// cout << "X(Xi)" << PROC.X(Xi).i() << endl;
+		// cout << "X(Xj)" << PROC.X(Xj).i() << endl;
+		// cout << "X(Xk)" << PROC.X(Xk).i() << endl;
+		// cout << "PROC.RA().u()" << PROC.RA().u() << endl;
+		// cout << "PROC.FL().u()" << PROC.FL().u() << endl;
+	    
+		assert(Xi < 16);
 		assert(Xj < 16);
 		assert(Xk < 16);
-		assert(PROC.X(Xj).u()+PROC.X(Xk).u() >= 256 * 32); // In legal memory region
-		assert(PROC.X(Xj).u()+PROC.X(Xk).u() < FreeMEM);   // In legal memory region
-		PROC.X(Xi).u() = MEM[PROC.X(Xj).u()+PROC.X(Xk).u()].u();
+
+		// Check upper bound and lower memory bound
+		// assert(PROC.X(Xj).u()+PROC.X(Xk).u() >=  PROC.RA().u()*256); // In legal memory region
+		// assert(PROC.X(Xj).u()+PROC.X(Xk).u() < PROC.FL().u()*256);   // In legal memory region
+		
+		PROC.X(Xi).u() = MEM[PROC.X(Xj).i()+PROC.X(Xk).i()].u();
+		
+		// cout << "X(Xi)" << PROC.X(Xi).i() << endl;
+		// cout << "X(Xj)" << PROC.X(Xj).i() << endl;
+		// cout << "X(Xk)" << PROC.X(Xk).i() << endl;
 	}
 
         void sdjki
@@ -58,7 +75,20 @@ namespace CDC8600
 	    uint8_t Xk
 	)
 	{
-	    assert(false);
+		// cout << "sdjki, Xi: " << unsigned(Xi) << "Xj: " << unsigned(Xj) << "Xk: " << unsigned(Xk) << endl;
+		// cout << "X(Xi)" << PROC.X(Xi).i() << endl;
+		// cout << "X(Xj)" << PROC.X(Xj).i() << endl;
+		// cout << "X(Xk)" << PROC.X(Xk).i() << endl;
+		assert(Xi < 16);
+		assert(Xj < 16);
+		assert(Xk < 16);
+
+		// Check for upper and lower memory bound: TODO
+
+	    MEM[PROC.X(Xj).i()+PROC.X(Xk).i()].u() = PROC.X(Xi).u();
+		// cout << "X(Xi)" << PROC.X(Xi).i() << endl;
+		// cout << "X(Xj)" << PROC.X(Xj).i() << endl;
+		// cout << "X(Xk)" << PROC.X(Xk).i() << endl;
 	}
 
         void isjki
@@ -68,7 +98,18 @@ namespace CDC8600
 	    uint8_t Xk
 	)
 	{
-	    assert(false);
+		// cout << "isjki, Xi: " << unsigned(Xi) << "Xj: " << unsigned(Xj) << "Xk: " << unsigned(Xk) << endl;
+	    assert(Xi < 16);
+		assert(Xj < 16);
+		assert(Xk < 16);
+		// cout << "X(Xi)" << PROC.X(Xi).u() << endl;
+		// cout << "X(Xj)" << PROC.X(Xj).u() << endl;
+		// cout << "X(Xk)" << PROC.X(Xk).i() << endl;
+		PROC.X(Xi).i() = PROC.X(Xj).i() + PROC.X(Xk).i();
+		// cout << "X(Xi)" << PROC.X(Xi).u() << endl;
+		// cout << "X(Xj)" << PROC.X(Xj).u() << endl;
+		// cout << "X(Xk)" << PROC.X(Xk).i() << endl;
+		
 	}
 	
         void idjkj
@@ -77,7 +118,9 @@ namespace CDC8600
 	    uint8_t k
 	)
 	{
-	    assert(false);
+		assert(Xj < 16);
+		assert(k < 16);
+	    PROC.X(Xj).u() = PROC.X(Xj).u() - k;
 	}
 
 	void idzkj
@@ -86,7 +129,9 @@ namespace CDC8600
 	   uint8_t Xk
 	)
 	{
-	    assert(false);
+		assert(Xj < 16);
+		assert(Xk < 16);
+		PROC.X(Xj).i() = 0-PROC.X(Xk).u();
 	}
 
 	void isjkj
@@ -95,7 +140,9 @@ namespace CDC8600
 	    uint8_t k
 	)
 	{
-	    assert(false);
+		assert(Xj < 16);
+		assert(k < 16);
+	    PROC.X(Xj).i() = PROC.X(Xj).i() + k;
 	}
 
 	void ipjkj
@@ -104,7 +151,9 @@ namespace CDC8600
 	    uint8_t Xk
 	)
 	{
-	    assert(false);
+		assert(Xj < 16);
+		assert(Xk < 16);
+		PROC.X(Xj).i() = PROC.X(Xj).i() * PROC.X(Xk).i();
 	}
 
 	void rdKj
