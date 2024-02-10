7b77667629002d30e64463ac9aed47dbd722dfcd
Jose Moreira <jmoreira@us.ibm.com>
2024-02-10 16:27:50 -0500

    formatting

--- Src/blas/drot.cc
+++ Src/blas/drot.cc
@@ -43,6 +43,7 @@ namespace CDC8600
         // f64 s		[ X6 ]
 	)
 	{
+            // clang-format off
             xkj(7, 0)		// X7 (ix) = 0
             xkj(8, 0)		// X8 (iy) = 0
             jmpp(2, L1)		// if X2 (incx) > 0 goto L1
@@ -56,19 +57,20 @@ LABEL(L1)   jmpp(4, loop)	// if X4 (incy) > 0 goto loop
 LABEL(loop) jmpz(0, end)	// if X0 (n) = 0 goto end
             rdjki(9, 1, 7)	// X9 (tmpx) = MEM[X1 (x) + X7 (ix)]
             rdjki(10, 3, 8)	// X10 (tmpy) = MEM[X3 (y) + X8 (iy)]
-            fmul(11, 5, 9)   // X11 (tmp1) = X9 (tmpx) * c
-            fmul(12, 6, 10)  // X12 (tmp2) = X10 (tmpy) * s
-            fadd(11, 11, 12) // X11 (tmp1) = X11 (tmp1) + X12 (tmp2)
+            fmul(11, 5, 9)   	// X11 (tmp1) = X9 (tmpx) * c
+            fmul(12, 6, 10)  	// X12 (tmp2) = X10 (tmpy) * s
+            fadd(11, 11, 12) 	// X11 (tmp1) = X11 (tmp1) + X12 (tmp2)
             sdjki(11, 1, 7)	// MEM[X1 (x) + X7 (ix)] = X11 (tmp1)
-            fmul(11, 5, 10)  // X11 (tmp1) = X10 (tmpy) * c
-            fmul(12, 6, 9)   // X12 (tmp2) = X9 (tmpx) * s
-            fsub(11, 11, 12) // X11 (tmp1) = X11 (tmp1) - X12 (tmp2)
+            fmul(11, 5, 10)  	// X11 (tmp1) = X10 (tmpy) * c
+            fmul(12, 6, 9)   	// X12 (tmp2) = X9 (tmpx) * s
+            fsub(11, 11, 12) 	// X11 (tmp1) = X11 (tmp1) - X12 (tmp2)
             sdjki(11, 3, 8)	// MEM[X3 (y) + X8 (iy)] = X11 (tmp11)
             isjki(7, 7, 2)	// X7 (ix) = X7 (ix) + X2 (incx)
             isjki(8, 8, 4)	// X8 (iy) = X8 (iy) + X4 (incy)
-	        idjkj(0, 1)		// X0 (n) = X0 (n) - 1
+	    idjkj(0, 1)		// X0 (n) = X0 (n) - 1
             jmp(loop)
 LABEL(end)  jmpk(15, 1)		// return to X15 (calling address) + 1
+            // clang-format on
 	}
     } // namespace BLAS
 } // namespace CDC8600
