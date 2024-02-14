a0e279150147ae58da26773f051fb498d12dc1d4
TsukiCU <hs3475@columbia.edu>
2024-02-13 22:09:15 -0500

    cpp version for dasum done

--- Src/blas/dasum.cc
+++ Src/blas/dasum.cc
@@ -1,3 +1,51 @@
 #include <CDC8600.hh>
 #include <ISA.hh>
-#include <blas/dcopy.hh>
+#include <blas/dasum.hh>
+
+namespace CDC8600
+{
+    namespace BLAS
+    {
+        f64 dasum(u64 n, f64 *dx, u64 incx)
+        {
+            return Func(dasum_cpp)(n, dx, incx);
+        }
+
+        f64 dasum_cpp(u64 n, f64 *dx, u64 incx)
+        {
+            assert(n > 0);
+            assert(incx > 0);
+
+            f64 dasum=0;
+            f64 dtemp=0;
+            u64 i, m;
+
+            if (incx == 1)
+            {
+                m = n % 6;
+                if (m != 0)
+                {
+                    for (i = 0; i < m; i++)
+                        dtemp += std::abs(dx[i]);
+                }
+                if (n < 6)
+                {
+                    dasum = dtemp;
+                    return dasum;
+                }
+                for (i = m; i < n; i += 6)
+                {
+                    dtemp += std::abs(dx[i]) + std::abs(dx[i+1]) + std::abs(dx[i+2]) +
+                    std::abs(dx[i+3]) + std::abs(dx[i+4])+ std::abs(dx[i+5]);
+                }
+            }
+            else
+            {
+                for (i = 0; i < n; i++)
+                    dtemp += std::abs(dx[i*incx]);
+            }
+            dasum = dtemp;
+            return dasum;
+        }
+    }
+}
--- Tests/dasum.cc
+++ Tests/dasum.cc
@@ -1,9 +1,50 @@
-#include <blas/zdotu.hh>
+#include <blas/dasum.hh>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
 #include <iomanip>
-#include <complex>
 
 using namespace CDC8600;
+
+extern "C" f64 dasum_(i32*, f64*, i32*);
+
+const int N = 20;
+const double epsilon = pow(1, -5);
+
+void test_dasum()
+{
+    reset();
+
+    int32_t n = rand() % 256;
+    int32_t incx = (rand() % 16);
+    uint32_t nx = n*abs(incx);  if(0 == nx) nx = 1;
+    bool pass = true;
+
+    // tracing = false;
+
+    f64 *dx = (f64*)CDC8600::memalloc(nx);
+
+    for (int i = 0; i < nx; i++) {dx[i] = drand48(); }
+
+    f64 ref = dasum_(&n, dx, &incx);
+    f64 ans = CDC8600::BLAS::dasum(n, dx, incx);
+
+    if ((ans-ref) > epsilon*ref) {pass = false; }
+
+    //cout << "dasum [" << setw(2) << count << "] (n = " << setw(3) << n << ", incx = " << setw(2) << incx << ", incy = " << setw(2) << incy << ", # of instr = " << setw(9) << instructions::count << ") : ";
+    if (n < 10) dump(trace);
+    if (pass)
+        cout << "PASS" << endl;
+    else
+        cout << "FAIL" << endl;
+}
+
+int main()
+{
+    for (int i = 0; i < N; i++)
+    {
+        test_dasum();
+    }
+    return 0;
+}
