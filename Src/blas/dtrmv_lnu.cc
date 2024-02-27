da9cbdbdc3193c1db59f0217e7059db940298a83
Anirudh-1149 <anirudh18@iitg.ac.in>
2024-02-27 18:28:55 -0500

    More temp changes

--- Src/blas/dtrmv_lnu.cc
+++ Src/blas/dtrmv_lnu.cc
@@ -14,18 +14,21 @@ namespace CDC8600
 
         void dtrmv_lnu_cpp(u64 n, f64* A, u64 lda, f64* X, i64 incx)
         {
-            // input validity check
             if(n == 0 || incx == 0 || lda < max((u64)1,n))
             {
                 return;
             }
 
-            i64 ix = 0;
+            i64 ix = (n-1)*incx;
+            if(incx < 0)
+            {
+                ix = 0;
+            }
             for(u32 i = n-1;i>0;i--)
             {
-                X[ix] = X[ix] +  ddot(i, X, incx, )
+                X[ix] = X[ix] +  ddot(i, X, incx, A + i, lda);
+                ix -= incx;
             }
-
         }
     }
 }
