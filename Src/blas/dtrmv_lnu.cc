df414be8c91125e2f94d26a5ff365cf304f74307
Anirudh-1149 <anirudh18@iitg.ac.in>
2024-02-27 00:02:23 -0500

    temp save

---
+++ Src/blas/dtrmv_lnu.cc
@@ -0,0 +1,31 @@
+#include <CDC8600.hh>
+#include <ISA.hh>
+#include <blas/dtrmv_lnu.hh>
+#include <blas/ddot.hh>
+
+namespace CDC8600
+{
+    namespace BLAS
+    {
+        void dtrmv_lnu(u64 n, f64* A, u64 lda, f64* X, i64 incx)
+        {
+            Call(dtrmv_lnu_cpp)(n, A, lda, X, incx);
+        }
+
+        void dtrmv_lnu_cpp(u64 n, f64* A, u64 lda, f64* X, i64 incx)
+        {
+            // input validity check
+            if(n == 0 || incx == 0 || lda < max((u64)1,n))
+            {
+                return;
+            }
+
+            i64 ix = 0;
+            for(u32 i = n-1;i>0;i--)
+            {
+                X[ix] = X[ix] +  ddot(i, X, incx, )
+            }
+
+        }
+    }
+}
