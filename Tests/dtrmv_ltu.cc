45b49c9b6683469764ad5c7c13fceba38a535384
tengjiang <jiangteng0403@gmail.com>
2024-02-29 20:57:25 -0500

    Fix dtrot, continue with dtrmv_ltu

--- Tests/dtrmv_ltu.cc
+++ Tests/dtrmv_ltu.cc
@@ -7,7 +7,21 @@
 
 using namespace CDC8600;
 
+extern "C" int32_t dtrmv_(char *, char *, char *, int32_t *, double *, int32_t *, double *, int32_t *);
+
+const u32 N = 20;
+
+void test_dcopy(int count)
+{
+
+    return;
+}
+
 int main()
 {
+    for (u32 i = 0; i < N; i++)
+    {
+        test_dcopy(i);
+    }
     return 0;
 }
