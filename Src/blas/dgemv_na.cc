bb1b502513ba7d4dc297f6866a9af60bef3ccc22
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air.local>
2024-03-01 19:11:28 -0500

    final code for hw4

--- Src/blas/dgemv_na.cc
+++ Src/blas/dgemv_na.cc
@@ -38,88 +38,20 @@ void dgemv_na_cpp(u64 m, u64 n, f64 alpha, f64* A, u64 lda, f64* x, i64 incx, f6
 
     if (m < 0 || n < 0 || (alpha == 0.0 && beta == 0.0) || incx == 0 || incy == 0)
     return;
-    //if (incy > 0 && incx>0 ) {
-        for (int i = 0; i<m; i++){
-            ix = 0;
-            if (incx <= 0) ix = (-n+1)*incx;  
-            for (int j = 0; j < n; ++j) {
-                temp1[i] = temp1[i]+ A[j*m+i]*x[ix];
-                ix += incx;
-            }
+    
+    for (int i = 0; i<m; i++){
+        ix = 0;
+        if (incx <= 0) ix = (-n+1)*incx;  
+        for (int j = 0; j < n; ++j) {
+            temp1[i] = temp1[i]+ A[j*m+i]*x[ix];
+            ix += incx;
         }
-        for (int i = 0; i< m; i++){
-            y[iy] = beta*y[iy];
-            iy += incy;
-        }
-        //daxpy_(&M, &beta, y, &INCY, temp2, &one);
-        daxpy_(&M, &alpha, temp1, &one, y, &INCY);
-       // daxpy_(&M, &fone, temp2, &one, y, &INCY);
-    //}
-
- /*      // Local Variables
-     double temp;
-     int i, info, ix, j, jy, kx;
-     const double zero = 0.0;
-
-     // Test the input parameters
-     info = 0;
-     if (m < 0)
-         info = 1;
-     else if (n < 0)
-         info = 2;
-     else if (incx == 0)
-         info = 5;
-     else if (incy == 0)
-         info = 7;
-     else if (lda < std::max(1, int(m)))
-         info = 9;
-
-     if (info != 0) {
-         char srname[] = "dger  ";
-         xerbla_(srname, &info);
-         return;
-     }
-
-     // Quick return if possible
-     if (m == 0 || n == 0 || alpha == zero)
-         return;
-
-     // Start the operations
-     if (incy > 0)
-         jy = 1;
-     else
-         jy = 1 - (n - 1) * incy;
-
-     if (incx == 1) {
-         for (j = 0; j < n; ++j) {
-             if (y[jy - 1] != zero) {
-                 temp = alpha * y[jy - 1];
-                 for (i = 0; i < m; ++i)
-                     a[i + j * lda] += x[i] * temp;
-             }
-             jy += incy;
-         }
-     } else {
-         if (incx > 0)
-             kx = 1;
-         else
-             kx = 1 - (m - 1) * incx;
-
-             //kx = (-m + 1) * incx;
-
-
-         for (j = 0; j < n; ++j) {
-             if (y[jy - 1] != zero) {
-                 temp = alpha * y[jy - 1];
-                 ix = kx;
-                 for (i = 0; i < m; ++i) {
-                     a[i + j * lda] += x[ix - 1] * temp;
-                     ix += incx;
-                 }
-             }
-             jy += incy;
-         }
-     } */ 
+    }
+    for (int i = 0; i< m; i++){
+        y[iy] = beta*y[iy];
+        iy += incy;
+    }
+    daxpy_(&M, &alpha, temp1, &one, y, &INCY);
  }
      } // namespace BLAS
  } // namespace CDC8600
