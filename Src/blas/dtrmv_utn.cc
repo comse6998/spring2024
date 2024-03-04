8a3dac98ba9d67fbe8efca385d9342c6548ea296
Manasmw01 <manas.pange@somaiya.edu>
2024-03-03 23:38:54 -0500

    INCX<0 modifications

--- Src/blas/dtrmv_utn.cc
+++ Src/blas/dtrmv_utn.cc
@@ -15,13 +15,17 @@ namespace CDC8600
         void dtrmv_utn_cpp(u64 n, f64* a, u64 lda, f64* x, i64 incx)
         
         {
-            if (n <= 0 || incx == 0 || lda <= 0 || lda < n)  return;
-            i64 ix = 0;
-            for (i64 i = n-1; i >= 0; i--) {
-                x[ix] = x[ix] + ddot((i), a + i, lda*i, x + ix,  incx);
-                cout << "n= " << (i) << ", dx= " << (i);
-                cout << ", incx= " << 1 << ", dy= " << (lda * i ) << ", incy= " << 1 << " ix = "<<  ix << endl;
-                ix += incx;
+            if (n <= 0 || incx == 0 || lda <= n)  return;
+            for (i32 i = n; i > 0; i--) {
+                i64 ix = (incx > 0 ? i : (-n+i)) * (incx);
+                if(incx > 0)
+                {
+                x[ix-incx] = ddot(i, x + ((incx > 0 ? 0 : (-i)*incx)), incx, a + ((i-1) * (lda)), 1);
+                }
+                else
+                {
+                x[ix] = ddot(i, x + ((incx > 0 ? 0 : ((-n+i)*incx))), incx, a + ((i-1) * (lda)), 1);
+                }
             }
         }
     } // namespace BLAS
