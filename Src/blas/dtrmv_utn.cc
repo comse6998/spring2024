93ebec4687087f17b2aea3453a3ac560a85da3e7
Manas Pange <72513954+Manasmw01@users.noreply.github.com>
2024-03-04 14:46:42 -0500

    Update dtrmv_utn.cc

--- Src/blas/dtrmv_utn.cc
+++ Src/blas/dtrmv_utn.cc
@@ -15,16 +15,16 @@ namespace CDC8600
         void dtrmv_utn_cpp(u64 n, f64* a, u64 lda, f64* x, i64 incx)
         
         {
-            if (n <= 0 || incx == 0 || lda <= n)  return;
+            if (n <= 0 || incx == 0 || lda < n)  return;
             for (i32 i = n; i > 0; i--) {
                 i64 ix = (incx > 0 ? i : (-n+i)) * (incx);
                 if(incx > 0)
                 {
-                x[ix-incx] = ddot(i, x + ((incx > 0 ? 0 : (-i)*incx)), incx, a + ((i-1) * (lda)), 1);
+                x[ix-incx] = ddot(i, x, incx, a + ((i-1) * (lda)), 1);
                 }
                 else
                 {
-                x[ix] = ddot(i, x + ((incx > 0 ? 0 : ((-n+i)*incx))), incx, a + ((i-1) * (lda)), 1);
+                x[ix] = ddot(i, x + ((-n+i)*incx), incx, a + ((i-1) * (lda)), 1);
                 }
             }
         }
