f8190f2539508426a4efbaf247557189a8b07d3b
tengjiang <jiangteng0403@gmail.com>
2024-03-05 21:51:30 -0500

    Reformatting.

--- Src/blas/dtrmv_ltu.cc
+++ Src/blas/dtrmv_ltu.cc
@@ -19,38 +19,38 @@ namespace CDC8600
             // Input check
             if (n <= 0 || incx == 0 || lda <= 0 || lda < n)
                 return;
-            u64 nx = (n-1) * abs(incx) + 1;
             f64 *tempx = (f64*)CDC8600::memalloc(n);
 
             // memcpy(tempx, x, nx * sizeof(f64)); // Not good, not tracked.
             
             // dcopy(n, x, 1, tempx, 1);
             // dcopy(n, x, incx, tempx, 1);
-
+            // dcopy(n, x, incx, tempx, (incx > 0) ? 1 : (-1));
+            
             #pragma omp parallel
             {
-                dcopy((n / nump()) + (me() < n % nump()),
-                (x + abs(incx) * me()),
-                abs (incx) * nump(),
-                tempx + me(),
-                nump());
+                if (nump() > 1)
+                    dcopy((n / nump()) + (me() < n % nump()),
+                    (x + abs(incx) * me()),
+                    abs(incx) * nump(),
+                    tempx + me(),
+                    nump());
             }
             
-
-            // #pragma omp parallel
-            // {
-            //     dcopy()
-            // }
-
             #pragma omp parallel
             {
                 for  (i64 i = me(); i < n - 1; i += nump()) {
                     i64 ix;
                     ix = (incx <= 0) ? (-n + 1 + i) * incx : (i * incx);
-                    x[ix] = x[ix] + ddot(n - i - 1,  A + lda * i + i + 1, 1, (incx > 0) ? (tempx + i + 1) : tempx, (incx > 0) ? 1 : (-1));
+                    if (nump() > 1)
+                        x[ix] = x[ix] + ddot(n - i - 1,  A + lda * i + i + 1, 1, (incx > 0) ? (tempx + i + 1) : tempx, (incx > 0) ? 1 : (-1));
+                    else
+                        x[ix] = x[ix] + ddot(n - i - 1,  A + lda * i + i + 1, 1, (incx < 0) ? x : x + ix + incx, incx);
+
                 }
             }
-            
+
+            // How to free memory?
         }
     } // namespace BLAS
 } // namespace CDC8600
