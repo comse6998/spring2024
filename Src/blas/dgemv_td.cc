074bbb6d16a11c9f2867b7e6bf3deeb29da4f810
Andy Cheng <atc2150@columbia.edu>
2024-03-08 16:57:25 -0500

    Add dgemv_td multithreaded support

--- Src/blas/dgemv_td.cc
+++ Src/blas/dgemv_td.cc
@@ -24,10 +24,10 @@ namespace CDC8600
 
             i64 iy = 0;
             if (incy <= 0) iy = (-n + 1) * incy;
-            for (i32 i = 0; i < n; ++i) {
-                y[iy] += alpha * ddot(m, x, incx, a + i*lda, 1);
-                iy += incy;
-            }
+
+#pragma omp parallel
+            for (u64 i = me(); i < n; i += nump())
+                y[iy + incy*i] += alpha * ddot(m, x, incx, a + i*lda, 1);
         }
     } // namespace BLAS
 } // namespace CDC8600
