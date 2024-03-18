4928fdbf549e9be2368ddd8564bf80770920a35e
Jose Moreira <jmoreira@us.ibm.com>
2024-03-18 14:02:59 -0400

    Split generating input and output data

--- Include/instructions/idzkj.hh
+++ Include/instructions/idzkj.hh
@@ -5,8 +5,9 @@ class idzkj : public Fjk
 
         bool execute()
         {
+            stringstream ss; ss << setfill('0') << setw(16) << hex << PROC[me()].X(_k).i();
             PROC[me()].X(_j).i() = -PROC[me()].X(_k).i();
-            stringstream ss; ss << setfill('0') << setw(16) << hex << PROC[me()].X(_k).i() << " " << PROC[me()].X(_j).i() << dec << setfill(' '); _trace = ss.str();
+	    ss << " " << PROC[me()].X(_j).i() << dec << setfill(' '); _trace = ss.str();
             return false;
         }
 
