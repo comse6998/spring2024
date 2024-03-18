6c8c3f0c5e16fc2f5bbcfc28dcb0fafafef7bb1b
Jose Moreira <jmoreira@us.ibm.com>
2024-03-17 21:47:26 -0400

    Started decoder

--- Include/instructions/idzkj.hh
+++ Include/instructions/idzkj.hh
@@ -1,23 +1,37 @@
 class idzkj : public Fjk
 {
     public:
-	idzkj(u08 j, u08 k) : Fjk(0x17, j, k) {}
+        idzkj(u08 j, u08 k) : Fjk(0x17, j, k) {}
 
-	bool execute()
-	{
-	    PROC[me()].X(_j).i() = -PROC[me()].X(_k).i();
-	    stringstream ss; ss << setfill('0') << setw(16) << hex << PROC[me()].X(_k).i() << " " << PROC[me()].X(_j).i() << dec << setfill(' '); _trace = ss.str();
-	    return false;
-	}
+        bool execute()
+        {
+            PROC[me()].X(_j).i() = -PROC[me()].X(_k).i();
+            stringstream ss; ss << setfill('0') << setw(16) << hex << PROC[me()].X(_k).i() << " " << PROC[me()].X(_j).i() << dec << setfill(' '); _trace = ss.str();
+            return false;
+        }
 
-	string mnemonic() const
-	{
-	    return "idzkj";
-	}
+        string mnemonic() const
+        {
+            return "idzkj";
+        }
 
-	bool ops()
-	{
-	    operations::process<operations::idzkj>(_j, _j, _k, 0);
-	    return false;
-	}
+        bool ops()
+        {
+            operations::process<operations::idzkj>(_j, _j, _k, 0);
+            return false;
+        }
+
+        bool match(u08 F)
+        {
+            if (0x17 == F) return true;
+            return false;
+        }
+
+        void decode(u32 code)
+        {
+            assert(code < 65536);       // 16-bit instruction
+            assert(match(code >> 8));   // we are in the right instruction
+            _k = code  & 0xf;           // extract the k field
+            _j = (code >> 4) & 0xf;     // extract the j field
+        }
 };
