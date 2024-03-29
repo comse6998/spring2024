d971a1458c397f751f2d5b4fb6d656e921c231c5
trev1no2 <jmt2235@columbia.edu>
2024-03-28 21:12:00 -0400

    Finished HW6

--- Include/instructions/rdjk.hh
+++ Include/instructions/rdjk.hh
@@ -5,12 +5,36 @@ class rdjk : public Fjk
 
 	bool execute()
 	{
-	    PROC[me()].X(_j).u() = PROC[me()].X(_k).u();
-            return false;
+		stringstream ss;
+		ss << setfill('0') << setw(16) << hex << PROC[me()].X(_k).i(); // combine the source data to the ss
+	    PROC[me()].X(_j).i() = PROC[me()].X(_k).i();
+		ss << " " << setw(16) << PROC[me()].X(_j).i() << dec << setfill(' ');
+		_trace = ss.str();
+        return false;
 	}
 
 	string mnemonic() const
 	{
 	    return "rdjk";
 	}
+
+	bool ops()
+        {
+            operations::process<operations::rdw>(_j, _k, 0);
+            return false;
+        }
+	
+	bool match(u08 F)
+        {
+            if (0x25 == F) return true;
+            return false;
+        }
+
+	void decode(u32 code)
+        {
+            assert(code < 65536);       // 16-bit instruction
+            assert(match(code >> 8));   // we are in the right instruction
+            _k = code  & 0xf;           // extract the k field
+            _j = (code >> 4) & 0xf;     // extract the j field
+        }
 };
