6b54f3fd85c4d62dd766faf1d21d4cd3dcd2278d
Jose Moreira <jmoreira@us.ibm.com>
2024-04-16 21:50:06 -0400

    A second argument specifies the maximum number of cycles

--- Tests/run.cc
+++ Tests/run.cc
@@ -10,21 +10,22 @@ using namespace std;
 
 int main
 (
-    int 	argc,
-    char**	argv
+    int         argc,
+    char**      argv
 )
 {
-    if (2 != argc)
+    if ((2 != argc) && (3 != argc))
     {
-	cerr << "Usage: " << argv[0] << " <trace file>" << endl;
-	return -1;
+        cerr << "Usage: " << argv[0] << " <trace file> [max # cycles]" << endl;
+        return -1;
     }
 
-    reset();				// reset the entire computer
-    instructions::initmakers();		// initialize the instruction makers
-    operations::initmappers();		// initialize the register mappers
-    pipeline::reset();			// reset the simulated pipeline
-    pipeline::run(argv[1]);		// run the trace through the pipeline
+    reset();                                                    // reset the entire computer
+    instructions::initmakers();                                 // initialize the instruction makers
+    operations::initmappers();                                  // initialize the register mappers
+    pipeline::reset();                                          // reset the simulated pipeline
+    if (2 == argc) pipeline::run(argv[1], UINT32_MAX);          // run the trace through the pipeline until completion
+    if (3 == argc) pipeline::run(argv[1], atoi(argv[2]));       // run the trace through the pipeline for a maximum of argv[2] cycles
 
     return 0;
 }
