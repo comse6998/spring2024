450b3a9a387a34edebe57878f8b0434382e8a9be
Jose Moreira <jmoreira@us.ibm.com>
2024-04-19 11:37:21 -0400

    supports command line (n, incx, incy)

--- Tests/ddot.cc
+++ Tests/ddot.cc
@@ -10,15 +10,14 @@ extern "C" f64 ddot_(i32*, f64 *, i32*, f64 *, i32*);
 
 const int N = 20;
 
-void test_ddot(int count)
+void test_ddot(int count, bool traceon, i32 n, i32 incx, i32 incy)
 {
     reset();
 
-    i32 n = rand() % 256;
-    i32 incx = (rand() % 16) - 8;
-    i32 incy = (rand() % 16) - 8;
     u32 dx = n*abs(incx); if (0 == dx) dx = 1;
     u32 dy = n*abs(incy); if (0 == dy) dy = 1;
+
+    tracing = traceon;
 
     f64 *x = (f64*)CDC8600::memalloc(dx*2);
     f64 *y = (f64*)CDC8600::memalloc(dy*2);
@@ -28,6 +27,7 @@ void test_ddot(int count)
 
     f64 ref_ = ddot_(&n, x, &incx, y, &incy);
     f64 new_ = CDC8600::BLAS::ddot(n, x, incx, y, incy);
+
     double epsilon = 1e-6;
     bool pass = true;
 
@@ -37,26 +37,53 @@ void test_ddot(int count)
         pass = false;
     }
 
-    std::cout << "ddot [" << std::setw(2) << count << "] (n = " << std::setw(3) << n << ", incx = " << std::setw(2) << incx << ", incy = " << std::setw(2) << incy << ", # of instr = " << std::setw(9) << PROC[0].instr_count << ") : ";
+    CDC8600::memfree(y, dx*2);
+    CDC8600::memfree(x, dy*2);
+
+    cout << "ddot [" << setw(2) << count << "] ";
+    cout << "(n = " << setw(3) << n;
+    cout << ", incx = " << setw(2) << incx;
+    cout << ", incy = " << setw(2) << incy;
+    cout << ", # of instr = "  << setw(9) << PROC[0].instr_count;
+    cout << ", # of ops = "    << setw(9) << PROC[0].op_count;
+    cout << ", # of cycles = " << setw(9) << PROC[0].op_maxcycle;
+    cout << ") : ";
     if (pass)
         std::cout << "PASS" << std::endl;
     else
         std::cout << "FAIL" << std::endl;
 
-    if (n < 10){
-        dump(PROC[0].trace);
-        dump(PROC[0].trace, "ddot.tr");
-    }
-
-    CDC8600::memfree(y, dx*2);
-    CDC8600::memfree(x, dy*2);
+    if (traceon) dump(PROC[0].trace);
+    if (traceon) dump(PROC[0].trace, "ddot.tr");
 }
 
-int main()
+int main
+(
+    int		argc,
+    char	**argv
+)
 {
-    for (int i = 0; i < N; i++)
+    if (1 == argc)
     {
-        test_ddot(i);
+	for (u32 i = 0; i < N; i++)
+	{
+	    i32 n = rand() % 256;
+	    i32 incx = (rand() % 16) - 8;
+	    i32 incy = (rand() % 16) - 8;
+	    test_ddot(i, false, n, incx, incy);
+	}
+    }
+    else if (4 == argc)
+    {
+	i32 n = atoi(argv[1]);
+	i32 incx = atoi(argv[2]);
+	i32 incy = atoi(argv[3]);
+	test_ddot(0, true, n, incx, incy);
+    }
+    else
+    {
+	cerr << "Usage : " << argv[0] << " [n incx incy]" << endl;
+	return -1;
     }
     return 0;
 }
