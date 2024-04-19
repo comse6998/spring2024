15e421b70efe4faefc28063bedf1353fca5630a7
Jose Moreira <jmoreira@us.ibm.com>
2024-04-19 11:41:15 -0400

    fixes

--- Tests/runtests.sh
+++ Tests/runtests.sh
@@ -11,11 +11,11 @@ done
 
 for i in ${BLAS1[@]}; 
 do 
-    ./$i  0  1  1 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
-    ./$i  1  1  1 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
-    ./$i 10  1  1 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
-    ./$i 10  2  3 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
-    ./$i 10 -2 -3 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
-    ./$i 10  2 -3 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
-    ./$i 10 -2  3 | grep $i && ./run $i.tr 1000 > $1.pipe && grep $1.tr $1.pipe
+    ./$i  0  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i  1  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10  2  3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10 -2 -3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10  2 -3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10 -2  3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
 done
