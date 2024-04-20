acb97bf56faddb7fd0cea7b17ee4dccaa173ba19
Jose Moreira <jmoreira@us.ibm.com>
2024-04-20 06:45:46 -0400

    formatting

--- Include/ISA.hh
+++ Include/ISA.hh
@@ -11,23 +11,23 @@
 
 #define LABEL(L) L : PROC[me()].instr_forcealign = __LINE__; addlabel(#L, __LINE__); 
 
-#define jmp(L)			if (process(new  instructions::jmp   (#L), __LINE__))      goto L;
+#define jmp(L)			if (process(new  instructions::jmp   (#L), __LINE__))     	goto L;
 
-#define jmpa(L)			if (process(new  instructions::jmpa  (0), __LINE__))      goto L;
+#define jmpa(L)			if (process(new  instructions::jmpa  (0), __LINE__))      	goto L;
 
-#define jmprng(Xj,L)		if (process(new instructions::jmprng (Xj,  0), __LINE__)) goto L;
+#define jmprng(Xj,L)		if (process(new instructions::jmprng (Xj,  0), __LINE__)) 	goto L;
 
-#define jmpnrng(Xj,L)		if (process(new instructions::jmpnrng(Xj,  0), __LINE__)) goto L;
+#define jmpnrng(Xj,L)		if (process(new instructions::jmpnrng(Xj,  0), __LINE__)) 	goto L;
 
-#define jmpz(Xj,L)		if (process(new instructions::jmpz   (Xj,  #L), __LINE__)) goto L;
+#define jmpz(Xj,L)		if (process(new instructions::jmpz   (Xj,  #L), __LINE__)) 	goto L;
 
-#define jmpnz(Xj,L)		if (process(new instructions::jmpnz  (Xj,  #L), __LINE__)) goto L;
+#define jmpnz(Xj,L)		if (process(new instructions::jmpnz  (Xj,  #L), __LINE__)) 	goto L;
 
-#define jmpp(Xj,L)		if (process(new instructions::jmpp   (Xj,  #L), __LINE__)) goto L;
+#define jmpp(Xj,L)		if (process(new instructions::jmpp   (Xj,  #L), __LINE__)) 	goto L;
 
-#define jmpn(Xj,L)		if (process(new instructions::jmpn   (Xj,  #L), __LINE__)) goto L;
+#define jmpn(Xj,L)		if (process(new instructions::jmpn   (Xj,  #L), __LINE__)) 	goto L;
 
-#define bb(Xj,Xk,L)		if (process(new instructions::bb     (Xj, Xk, #L), __LINE__)) goto L;
+#define bb(Xj,Xk,L)		if (process(new instructions::bb     (Xj, Xk, #L), __LINE__)) 	goto L;
 
 #define call(Xj,func)		if (process(new instructions::call   (Xj), __LINE__))     func ();
 
@@ -37,13 +37,13 @@
 
 #define jmpk(Xj,k)		if (process(new instructions::jmpk   (Xj, k), __LINE__))  return;
 
-#define jmpk0(Xj,k)           if (process(new instructions::jmpk0   (Xj, k), __LINE__))  return 0;
+#define jmpk0(Xj,k)             if (process(new instructions::jmpk0  (Xj, k), __LINE__))  return 0;
 
 // 2. Load/store facility
 
 #define rdjki(Xi, Xj, Xk)	process(new instructions::rdjki(Xi, Xj, Xk), __LINE__);
 
-#define rdjk(Xj, Xk)  process(new instructions::rdjk(Xj, Xk), __LINE__);
+#define rdjk(Xj, Xk)            process(new instructions::rdjk(Xj, Xk), __LINE__);
 
 #define sdjki(Xi, Xj, Xk)	process(new instructions::sdjki(Xi, Xj, Xk), __LINE__);
 
@@ -76,6 +76,5 @@
 #define fadd(Xi, Xj, Xk)	process(new instructions::fadd(Xi, Xj, Xk), __LINE__);
 
 #define fsub(Xi, Xj, Xk)	process(new instructions::fsub(Xi, Xj, Xk), __LINE__);
-
 
 #endif // _ISA_HH_
