1715ce02c63aa6a65a87d80ef4b17ac5f3635fd2
Jose Moreira <jmoreira@us.ibm.com>
2024-04-21 17:52:08 -0400

    scroll pipes go to .pipe files automatically

--- Tests/runtests.sh
+++ Tests/runtests.sh
@@ -11,11 +11,11 @@ done
 
 for i in ${BLAS1[@]}; 
 do 
-    ./$i  0  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
-    ./$i  1  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
-    ./$i 10  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
-    ./$i 10  2  3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
-    ./$i 10 -2 -3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
-    ./$i 10  2 -3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
-    ./$i 10 -2  3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i  0  1  1 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i  1  1  1 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i 10  1  1 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i 10  2  3 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i 10 -2 -3 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i 10  2 -3 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i 10 -2  3 | grep $i && ./run $i 1000 && grep $i $i.pipe
 done
