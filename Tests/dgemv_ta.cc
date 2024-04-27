083e0b7e92e1582c4818e4c36d70b388b87058f9
xh2373 <xh2373@columbia.edu>
2024-04-27 19:00:50 -0400

    dgmev_ta with input arguments

--- Tests/dgemv_ta.cc
+++ Tests/dgemv_ta.cc
@@ -10,50 +10,34 @@ using namespace CDC8600;
 
 extern "C" int32_t dgemv_(char *trans, int32_t *m, int32_t *n, double *alpha, double *A, int32_t *LDA, double *x, int32_t *incx, double *beta, double *y, int32_t *incy);
 
-const int N = 4;
+const int N = 20;
 
-void test_dgemv_ta(int count, bool tracing)
+void test_dgemv_ta(int count, int traceon, i32 m, i32 n, i32 LDA, f64 alpha, f64 beta, i32 incx, i32 incy)
 {
+    
     reset();
 
-    int32_t m = rand() % 256;
-    int32_t n = rand() % 256;
-    int32_t LDA = m + rand() % 256; if (0 == LDA) LDA = 1;
-    //int32_t LDA = m;
-    double alpha = f64(drand48());
-    double beta = f64(drand48());
-    int32_t incx = (rand() % 16) - 8; if (0 == incx) incx = 1;
-    int32_t incy = (rand() % 16) - 8; if (0 == incy) incy = 1;
-    //int32_t incx = 1;
-    //int32_t incy = 1;
+    tracing = traceon;
     
-    if (tracing) {
-      m = 3;
-      n = 3;
-      LDA = 8;
-      incx = 1;
-      incy = 1;
-    }
-
-    int32_t nx = m*abs(incx); if (0 == nx) nx = 1;
-    int32_t ny = n*abs(incy); if (0 == ny) ny = 1;
+    i32 nx = m*abs(incx); if (0 == nx) nx = 1;
+    i32 ny = n*abs(incy); if (0 == ny) ny = 1;
 
     f64 *A = (f64*)CDC8600::memalloc(LDA*n);
     f64 *x = (f64*)CDC8600::memalloc(nx);
     f64 *y = (f64*)CDC8600::memalloc(ny);
     f64 *Y = new f64[ny];
 
-    for (int32_t i = 0; i < LDA*n; i++) { A[i] = f64(drand48()); }
-    for (int32_t i = 0; i < nx; i++) { x[i] = f64(drand48()); }
-    for (int32_t i = 0; i < ny; i++) { y[i] = f64(drand48()); }
-    for (int32_t i = 0; i < ny; i++) { Y[i] = y[i]; }
+    for (int i = 0; i < LDA*n; i++) { A[i] = f64(drand48()); }
+    for (int i = 0; i < nx; i++) { x[i] = f64(drand48()); }
+    for (int i = 0; i < ny; i++) { y[i] = f64(drand48()); }
+    for (int i = 0; i < ny; i++) { Y[i] = y[i]; }
 
     char trans = 'T';
 
     dgemv_(&trans, &m, &n, &alpha, A, &LDA, x, &incx, &beta, Y, &incy);         // Reference implementation of DAXPY
     CDC8600::BLAS::dgemv_ta(m, n, alpha, A, LDA, x, incx, beta, y, incy);       // Implementation of DAXPY for the CDC8600
     
-    const double epsilon = 1e-9;
+    const f64 epsilon = 1e-9;
     bool pass = true;
     for (int i = 0; i < ny; i++)
     {
@@ -87,33 +71,48 @@ void test_dgemv_ta(int count, bool tracing)
         cout << "FAIL" << std::endl;
 
     if (tracing) {
-      cout << "\n\n PROC[0] trace ========================== \n\n";
-      dump(PROC[0].trace);
-      cout << "\n\n PROC[1] trace ========================== \n\n";
-      dump(PROC[1].trace);
-      cout << "\n\n PROC[2] trace ========================== \n\n";
-      dump(PROC[2].trace);
-      cout << "\n\n PROC[3] trace ========================== \n\n";
-      dump(PROC[3].trace);
-      cout << "\n\n ========================== \n\n";
       dump(PROC[0].trace, "dgemv_ta.tr.0");
-      cout << "\n\n ++++++++++++++++++++++++++ \n\n";
       dump(PROC[1].trace, "dgemv_ta.tr.1");
-      cout << "\n\n ++++++++++++++++++++++++++ \n\n";
       dump(PROC[2].trace, "dgemv_ta.tr.2");
-      cout << "\n\n ++++++++++++++++++++++++++ \n\n";
       dump(PROC[3].trace, "dgemv_ta.tr.3");
-      cout << "\n\n ++++++++++++++++++++++++++ \n\n";
     }
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    for (int i = 0; i < N; i++)
+    if (argc == 1)
     {
-        test_dgemv_ta(i, false);
+	for (u32 i = 0; i < N; i++)
+	{
+            i32 m = rand() % 256;
+            i32 n = rand() % 256;
+            i32 LDA = m + rand() % 256; if (0 == LDA) LDA = 1;
+            f64 alpha = f64(drand48());            
+            f64 beta = f64(drand48());           
+            i32 incx = (rand() % 16) - 8; if (incx == 0) incx = 1;
+            i32 incy = (rand() % 16) - 8; if (incy == 0) incy = 1;
+                        
+	    test_dgemv_ta(i, false, m, n, LDA, alpha, beta, incx, incy);
+	}
     }
-
-    test_dgemv_ta(0, true);
-    return 0;
+    else if (argc == 8)
+    {
+        i32 m = atoi(argv[1]);
+	i32 n = atoi(argv[2]);
+        i32 LDA = atoi(argv[3]);
+	f64 alpha = atof(argv[4]);
+	f64 beta = atof(argv[5]);
+	i32 incx = atoi(argv[6]);
+	i32 incy = atoi(argv[7]);
+        if (LDA < m) {
+            cerr << "LDA must be larger than m" << endl;
+            exit(1);
+        }
+        test_dgemv_ta(0, true, m, n, LDA, alpha, beta, incx, incy);
+    }
+    else
+    {
+	cerr << "Usage : " << argv[0] << " [m n LDA alpha beta incx incy]" << endl;
+	exit(1);
+    }
 }
