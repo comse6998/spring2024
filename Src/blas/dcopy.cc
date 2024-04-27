621a4a5430db09d369ebc6ff6e774cbfe76942f4
Choka Thenappan <choka@Chokas-MacBook-Air.local>
2024-04-27 14:56:35 -0400

    HW9

--- Src/blas/dcopy.cc
+++ Src/blas/dcopy.cc
@@ -39,27 +39,34 @@ namespace CDC8600
             xkj(5, 0)           // X5 (ix) = 0
             xkj(6, 0)           // X6 (iy) = 0
             jmpp(2, L1)         // if X2 (incx) > 0 goto L1
+
             idzkj(5, 0)         // X5 (ix) = -X0 (n)
             isjkj(5, 1)         // X5 (ix) = X5(-n) + 1
             ipjkj(5, 2)         // X5 (ix) = X5 (-n+1) * X2 (incx)
             pass()              // padding
+
 LABEL(L1)   pass()              // padding
             pass()              // padding
             jmpp(4, loop)       // if X4 (incy) > 0 goto loop
+
             idzkj(6, 0)         // X6 (iy) = -X0 (n)
             isjkj(6, 1)         // X6 (iy) = X6(-n) + 1
             ipjkj(6, 4)         // X6 (iy) = X6 (-n+1) * X4 (incy)
             pass()              // padding
+
 LABEL(loop) pass()              // padding
             pass()              // padding
             jmpz(0, end)        // if X0 (n) = 0 goto end
+
             rdjki(7, 1, 5)      // X7 (tmp) = MEM[X1 (x) + X5 (ix)]
             sdjki(7, 3, 6)      // MEM[X3 (y) + X6 (iy)] = X7 (tmp)
             isjki(5, 5, 2)      // X5 (ix) = X5 (ix) + X2 (incx)
             isjki(6, 6, 4)      // X6 (iy) = X6 (iy) + X4 (incy)
+
             idjkj(0, 1)         // X0 (n) = X0 (n) - 1
             pass()              // padding
             jmp(loop)           // repeat
+            
 LABEL(end)  pass()              // padding
             pass()              // padding
             pass()              // padding
