d0b0e709001630939f04e7cc4eb81268336324f4
yl5334 <yl5334@columbia.edu>
2024-04-26 22:28:20 -0400

    dtrmv_ltn timing-driven trace

--- Tests/dtrmv_ltn.cc
+++ Tests/dtrmv_ltn.cc
@@ -18,7 +18,7 @@ char UPLO = 'L'; char TRANS = 'T'; char DIAG = 'N';
 void test_dtrmv_ltn(int count, bool traceon, i32 n, i32 LDA, i32 incx)
 {
     reset();
-    tracing = traceon;
+    //tracing = traceon;
     //i32 n = rand() % 256;
     //i32 incx = (rand() % 16) - 8;
     //if (incx == 0) incx = 1;
@@ -44,8 +44,18 @@ void test_dtrmv_ltn(int count, bool traceon, i32 n, i32 LDA, i32 incx)
         }
     }
     delete [] x;
-    //CDC8600::memfree(A, nA);
-    CDC8600::memfree(X, DX);
+    if (traceon)
+    {
+       for (u32 p = 0; p < params::Proc::N; p++) {
+        cout << "PROC[" << p << "], # of instruction = " << setw(9) << PROC[p].instr_count << endl;
+        dump(PROC[p].trace);
+        dump(PROC[p].trace, ("dtrmv_ltn.tr." + std::to_string(p)).c_str());
+        cout << '\n';
+        cout << "# of cycles = ";
+        cout << setw(9) << PROC[p].op_maxcycle << endl;
+       }
+    }
+    
     cout << "dtrmv_ltn [" << setw(2) << count << "] ";
     cout << "(lda = " << setw(3) << LDA;
     cout << ", n = " << setw(3) << n;
@@ -70,17 +80,10 @@ void test_dtrmv_ltn(int count, bool traceon, i32 n, i32 LDA, i32 incx)
     cout << "Cache Setting ( Line size = " << params::L1::linesize << ", nsets = "<< params::L1::nsets ;
     cout << ", nways = "<< params::L1::nways << ", latency = " << params::L1::latency << ")" << std::endl;
     */
-    if (traceon)
-    {
-       for (u32 p = 0; p < params::Proc::N; p++) {
-        cout << "PROC[" << p << "], # of instruction = " << setw(9) << PROC[p].instr_count << endl;
-        dump(PROC[p].trace);
-        dump(PROC[p].trace, ("dtrmv_ltn.tr." + to_string(p)).c_str());
-        cout << '\n';
-        cout << "# of cycles = ";
-        cout << setw(9) << PROC[p].op_maxcycle << endl;
-       }
-    }
+    
+
+    CDC8600::memfree(A, nA);
+    CDC8600::memfree(X, DX);
     
     /*
     if (Pass)
@@ -92,7 +95,7 @@ void test_dtrmv_ltn(int count, bool traceon, i32 n, i32 LDA, i32 incx)
 }
 int main(int argc, char **argv)
 {
-    if (1 == argc)
+    if (argc == 1)
     {
         for (int i = 0; i < N; i++)
     {
@@ -100,6 +103,7 @@ int main(int argc, char **argv)
         i32 incx = (rand() % 16) - 8;
         if (incx == 0) incx = 1;
         i32 LDA = n + rand()%256;
+        LDA = max(LDA, 1);
         test_dtrmv_ltn(i, false, n, LDA, incx);
     }
     }
@@ -109,6 +113,14 @@ int main(int argc, char **argv)
         i32 LDA = atoi(argv[2]);
         i32 incx = atoi(argv[3]);
 
+        if(incx<1) {
+            cout << "incx must > 1" <<endl;
+            exit(1);
+        }
+        if (LDA < n || LDA < 1) {
+            cout << "LDA must be equal to or larger than 1 or n" << endl;
+            exit(1);
+        }
         test_dtrmv_ltn(0, true, n, LDA, incx);
     }
     else
