0951fc2a78cf4a415456aa067e5e8780c4e4ef94
Anirudh-1149 <av3162@columbia.edu>
2024-05-01 20:07:44 -0400

    modified dtrmv_lnu

--- Tests/dtrmv_lnu.cc
+++ Tests/dtrmv_lnu.cc
@@ -16,20 +16,15 @@ char UPLO = 'L';
 char TRANS = 'N';
 char DIAG = 'U';
 
-void test_dtrmv_lnu(int count)
+void test_dtrmv_lnu(int count, bool traceon, i32 n, i32 incx)
 {
     reset();
-
-    i32 n = rand() % 256;
-    i32 incx = (rand() % 16) - 8;
     if (incx == 0) incx = 1;
-    // how big should lda be than n ?? 
-    i32 lda = n + rand()%256;
+    i32 lda = n + rand()%100;
     
     u32 nx = n*abs(incx); if (0 == nx) nx = 1;
     u32 na = n*lda;
-    tracing = false; 
-    if (n < 10) tracing = true;
+    tracing = traceon;
 
     f64 *A = (f64*)CDC8600::memalloc(na);
     f64 *X = (f64*)CDC8600::memalloc(nx);
@@ -54,36 +49,50 @@ void test_dtrmv_lnu(int count)
 
     delete [] x;
 
-    cout << "dtrmv_lmu [" << setw(2) << count << "] ";
+    cout << "dtrmv_lnu [" << setw(2) << count << "] ";
     cout << "(n = " << setw(3) << n;
-    cout << ", incx = " << setw(2) << incx;
-    cout << ", lda = " << setw(3) << lda;
-    cout << ", # of instr = ";
-    for (u32 p = 0; p < params::Proc::N; p++) cout << setw(9) << PROC[p].instr_count;
-    cout << ", # of cycles = ";
-    for (u32 p = 0; p < params::Proc::N; p++) cout << setw(9) << PROC[p].op_maxcycle;
+    cout << ", incx = " << setw(3) << incx;
+    cout << ", # of instr  = ("; cout << setw(9) << PROC[0].instr_count; for (u32 i = 1; i < params::Proc::N; i++) cout << ", " << setw(9) << PROC[i].instr_count; cout << ")";
+    cout << ", # of ops    = ("; cout << setw(9) << PROC[0].op_count   ; for (u32 i = 1; i < params::Proc::N; i++) cout << ", " << setw(9) << PROC[i].op_count   ; cout << ")";
+    cout << ", # of cycles = ("; cout << setw(9) << PROC[0].op_maxcycle; for (u32 i = 1; i < params::Proc::N; i++) cout << ", " << setw(9) << PROC[i].op_maxcycle; cout << ")";
     cout << ") : ";
     if (pass)
         cout << "PASS" << std::endl;
     else
         cout << "FAIL" << std::endl;
-    
-    if (n < 10)
-    {
-        dump(PROC[0].trace, "dtrmv_lnu.tr.0");
-        dump(PROC[1].trace, "dtrmv_lnu.tr.1");
-        dump(PROC[2].trace, "dtrmv_lnu.tr.2");
-        dump(PROC[3].trace, "dtrmv_lnu.tr.3");
-    } 
+
+    if (traceon) dump(PROC[0].trace, "dtrmv_lnu.tr.0");
+    if (traceon) dump(PROC[1].trace, "dtrmv_lnu.tr.1");
+    if (traceon) dump(PROC[2].trace, "dtrmv_lnu.tr.2");
+    if (traceon) dump(PROC[3].trace, "dtrmv_lnu.tr.3");
 
 }
 
 
-int main()
+int main(
+    int         argc,
+    char        **argv
+)
 {
-    for (int i = 0; i < N; i++)
+    if(argc == 1)
     {
-        test_dtrmv_lnu(i);
+        for (u32 i = 0; i < N; i++)
+        {
+            i32 n = rand() % 100;
+            i32 incx = (rand() % 16 - 8) ;
+            test_dtrmv_lnu(i, false, n, incx);
+        }
+    }
+    else if(argc == 3)
+    {
+        i32 n = atoi(argv[1]);
+        i32 incx = atoi(argv[2]);
+        test_dtrmv_lnu(0,true, n, incx);
+    }
+    else
+    {
+        cerr << "Usage : " << argv[0] << " [n incx]" << endl;
+        return -1;
     }
     return 0;
 }
