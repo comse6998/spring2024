92a80beaa7943639c1653fc168293a0c0ff27627
Manasmw01 <manas.pange@somaiya.edu>
2024-05-05 18:51:18 -0400

    HW10 cmd line arguments

--- Tests/dtrmv_utn.cc
+++ Tests/dtrmv_utn.cc
@@ -13,13 +13,14 @@ extern "C" i32 dtrmv_(char *, char *, char *, i32 *, f64 *, i32 *, f64 *, i32 *)
 const int N = 20;
 const double EPSILON = 1e-9;
 
-void test_dtrmv_utn(int count, bool traceon,i32 n, i32 lda, i32 incx)
+void test_dtrmv_utn(int count, bool traceon,i32 n, i32 incx)
 {
     reset();
     char uplo = 'U', trans = 'T', diag = 'N';
+    i32 lda = n + rand() % 256;
+    lda = max(lda, 1);
 
     tracing = traceon;
-    if (n < 10) tracing = true;
     f64 *A = (f64*)CDC8600::memalloc(n*lda);
     f64 *X = (f64*)CDC8600::memalloc(n*abs(incx));    // Input matrix
     f64 *Y = new f64[n*abs(incx)];                    // Reference matrix
@@ -45,9 +46,11 @@ void test_dtrmv_utn(int count, bool traceon,i32 n, i32 lda, i32 incx)
     cout << "(lda = " << setw(3) << lda;
     cout << ", n = " << setw(3) << n;
     cout << ", incx = " << setw(3) << incx;
-    cout << ", # of instr = ";
+    cout << ", # of instr   = (";
     for (u32 p = 0; p < params::Proc::N; p++) cout << setw(9) << PROC[p].instr_count;
-    cout << ", # of cycles = ";
+    cout << "), # of ops    = (";
+    for (u32 p = 0; p < params::Proc::N; p++) cout << setw(9) << PROC[p].op_count;
+    cout << "), # of cycles = (";
     for (u32 p = 0; p < params::Proc::N; p++) cout << setw(9) << PROC[p].op_maxcycle;
     cout << ") : ";
     if (pass)
@@ -73,22 +76,25 @@ char	**argv)
         for (int i = 0; i < N; i++)
         {
             i32 n = rand() % 256;
-            i32 lda = n + rand() % 256;
             i32 incx = (rand() % 16) - 8; if (incx == 0) incx = 1;
-            test_dtrmv_utn(i, false, n, lda, incx);
+            test_dtrmv_utn(i, false, n, incx);
         }
     }
-    else if(argc == 4)
+    else if(argc == 3)
     {
     	i32 n = atoi(argv[1]);
-        i32 lda = atoi(argv[2]);
-    	i32 incx = atoi(argv[3]);
-        test_dtrmv_utn(0, true, n, lda, incx);
+    	i32 incx = atoi(argv[2]);
+        if (incx == 0) {
+            cerr << "incx must be non zero" << endl;
+        	cerr << "Usage : " << argv[0] << " [n incx]" << endl;
+	        return -1;        
+        }
+        test_dtrmv_utn(0, true, n, incx);
 
     }
     else
     {
-	cerr << "Usage : " << argv[0] << " [n lda incx]" << endl;
+	cerr << "Usage : " << argv[0] << " [n incx]" << endl;
 	return -1;        
     }
     return 0;
