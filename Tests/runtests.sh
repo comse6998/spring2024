2d58ecca0d4ab9bdbbbdc7b5a7a019e186387bd7
Gaurav Agarwal <gaurav@Gauravs-MacBook-Air-2.local>
2024-05-12 23:36:19 -0400

    final push

--- Tests/runtests.sh
+++ Tests/runtests.sh
@@ -1,10 +1,6 @@
 #!/bin/ksh
 
-if [ $# -gt 0 ]; then
-    set -A BLAS1 "$@"
-else
-    set -A BLAS1 "daxpy dcopy ddot dscal zaxpy zdotc zdotu zdrot"
-fi
+set -A BLAS1 "daxpy"
 
 make clean && clear
 make run
@@ -15,11 +11,11 @@ done
 
 for i in ${BLAS1[@]}; 
 do 
-    ./$i  0  1  1 | grep $i && ./run $i 1000 && grep $i $i.pipe
-    ./$i  1  1  1 | grep $i && ./run $i 1000 && grep $i $i.pipe
-    ./$i 10  1  1 | grep $i && ./run $i 1000 && grep $i $i.pipe
-    ./$i 10  2  3 | grep $i && ./run $i 1000 && grep $i $i.pipe
-    ./$i 10 -2 -3 | grep $i && ./run $i 1000 && grep $i $i.pipe
-    ./$i 10  2 -3 | grep $i && ./run $i 1000 && grep $i $i.pipe
-    ./$i 10 -2  3 | grep $i && ./run $i 1000 && grep $i $i.pipe
+    ./$i  0  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i  1  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10  1  1 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10  2  3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10 -2 -3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10  2 -3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
+    ./$i 10 -2  3 | grep $i && ./run $i.tr 1000 > $i.pipe && grep $i.tr $i.pipe
 done
